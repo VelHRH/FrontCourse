e69d845db7bff17fc0006ed54e40b48fa76b0b53
Valentine Yurchenko <45362610+VelHRH@users.noreply.github.com>
2022-10-16 22:59:42 +0300

    First half of HV7

---
+++ Homework 7/script.js
@@ -0,0 +1,129 @@
+//  1
+// let admin, name;
+// name = "Valentin";
+// admin = name;
+// alert(admin);
+
+//  2
+//  BIRTHDAY - так, бо дата народження є незмінною, AGE - ні, бо в залежності від року вік змінюється
+
+//  3
+  // let name = "Ilya " ;
+  // alert ( ` hello ${ 1 } ` ); // hello 1
+  // alert ( ` hello ${ "name " } ` ); // hello name
+  // alert ( ` hello ${ name } ` ); // hello Ilya
+
+//  4
+// "" + 1 + 0 //10
+// "" - 1 + 0 //-1
+// true + false; //1
+// 6 / "3" //2
+// "2" * "3" //6
+// 4 + 5 + "px" //9px
+// "$" + 4 + 5 //$45
+// "4" - 2 //2
+// "4px" - 2; //NaN
+// 7 / 0 //Infinity
+// " – 9 " + 5 //– 9 5
+// " – 9 " - 5 //NaN
+// null + 1 //1
+// undefined + 1 //NaN
+
+//  5
+// let a = 1, b = 1;
+// let c = ++a; // c=2 a=2
+// let d = b++; // d=1 b=2
+
+//  6
+// let a = 2;
+// let x = 1 + (a *= 2 ); //a=4 x=5
+
+//  7
+// 5>4 //true
+// "ананас" > "яблуко" //false
+// "2" > "12" //true
+// undefined == null //true
+// undefined === null //false
+// null == "\n0\n" //false
+// null === +"\n0\n " //false
+
+//  8
+// let name = prompt("Your name?");
+// alert(name);
+
+//  9
+// Так, бо тут 0 у вигляді строки
+
+//  10
+// let a = prompt("Яка «офіційна» назва JavaScript ?");
+// if (a=="ECMAScript"){
+//   alert("Правильно!");
+// }
+// else{
+//   alert("Не знаєте? ECMAScript!");
+// }
+
+//  11
+// let num = prompt();
+// if (num>0){
+//   alert(1);
+// }
+// if (num < 0){
+//   alert(-1);
+// }
+// if (num ==0){
+//   alert(0);
+// }
+
+//  12
+// let result;
+// result = a + b < 4 ? ' Мало ' : 'Багато';
+
+//  13
+// let message;
+// message = login == ' Співробітник' ? ' Привіт ' : 
+//   login == 'Директор' ? ' Добрий день! ' : 
+//   login == '' ? 'Відсутній логін' : 
+//   '';
+
+//  14
+// alert( null || 2 || undefined ); //2
+
+//  15
+// alert(alert (1) || 2 || alert(3)) // 1 та 2
+
+//  16
+// alert(1 && null && 2); //null
+
+//  17
+// alert(alert(1) && alert(2)); // 1 та undefined
+
+//  18
+// alert(null || 2 && 3 || 4 ); //3
+
+//  19
+// if (age>=14 && age <=90){
+//   alert("Yes");
+// }
+// else{
+//   alert("No");
+// }
+
+//  20
+// if (!(age>=14 && age <=90)){
+//   alert("Yes");
+// }
+// else{
+//   alert("No");
+// }
+// if (age>=14 && age <=90){
+//   alert("No");
+// }
+// else{
+//   alert("Yes");
+// }
+
+//  21
+// if (-1 || 0) alert( 'first'); //true, -1
+// if (-1 && 0) alert( 'second' ); //false, 0
+// if (null || -1 && 1) alert('third' ); //true, 1
