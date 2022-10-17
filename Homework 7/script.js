f68f868ce4616f25d0c7e382d3dfca912a80a9a8
Valentine Yurchenko <45362610+VelHRH@users.noreply.github.com>
2022-10-17 11:38:07 +0300

    HV7 done

--- Homework 7/script.js
+++ Homework 7/script.js
@@ -127,3 +127,244 @@
 // if (-1 || 0) alert( 'first'); //true, -1
 // if (-1 && 0) alert( 'second' ); //false, 0
 // if (null || -1 && 1) alert('third' ); //true, 1
+
+//  22
+// let login = prompt("Ваш логін");
+// if (login=="Адмін"){
+//   let pass = prompt("Пароль");
+//   if (pass == "я головний") { 
+//     alert("Здрастуйте!")
+//   } else if (pass == null || pass == ""){ 
+//     alert("Скасовано")
+//   } else {
+//     alert("Невірний пароль");
+//   }
+// } 
+// else if (login == null || login==""){
+//   alert("Скасовано");
+// }
+// else {
+//   alert("Я вас не знаю");
+// }
+
+//  23
+// if (browser == 'Edge') {
+//    alert(" You've got the Edge! ");
+// } else if (browser == 'Chrome' || browser == 'Firefox' || browser == 'Safari' || browser == 'Opera'){
+//    alert( ' Okay we support these browsers too' );
+// } else {
+//    alert( ' We hope that this page looks ok!' );
+// }
+
+//  24
+// let a = +prompt( 'a?', '');
+// switch (a){
+//   case (0):
+//     alert(0);
+//     break;
+//   case (1):
+//     alert(1);
+//     break;
+//   case (2):
+//     alert('2,3');
+//     break;
+//   case (3):
+//     alert('2,3');
+//     break;
+//   default:
+//     break;
+// }
+
+//  25
+// let a = Number(prompt());
+// a = a>0 ? ++a : a;
+// alert(a);
+
+//  26
+// let a = Number(prompt());
+// a = a>0 ? ++a : a-2;
+// alert(a);
+
+//  27
+// let a = Number(prompt());
+// a = a>0 ? ++a : a<0 ? a-2 : 10;
+// alert(a);
+
+//  28
+// let a = Number(prompt());
+// let b = Number(prompt());
+// let c = Number(prompt());
+// let res = 0;
+// if (a>0) res++;
+// if (b>0) res++;
+// if (c>0) res++;
+// alert(res);
+
+//  29
+// let a = Number(prompt());
+// let b = Number(prompt());
+// let c = Number(prompt());
+// let pos = 0;
+// let neg = 0;
+// if (a>0) pos++;
+// if (b>0) pos++;
+// if (c>0) pos++;
+// if (a<0) neg++;
+// if (b<0) neg++;
+// if (c<0) neg++;
+// alert("Positive - " + pos);
+// alert("Negative - " + neg);
+
+//  30
+// let a = Number(prompt("A:"));
+// let b = Number(prompt("B:"));
+// if (a>b) {alert("A");}
+// else if (b>a) {alert("B");}
+// else {
+//   alert("Equal input");
+// }
+
+//  31
+// let a = Number(prompt("A:"));
+// let b = Number(prompt("B:"));
+// if (a>b) {alert("B");}
+// else if (b>a) {alert("A");}
+// else {
+//   alert("Equal input");
+// }
+
+//  32
+// let a = Number(prompt("A:"));
+// let b = Number(prompt("B:"));
+// if (a<b) {alert("B"); alert("A");}
+// else if (b<a) {alert("A"); alert("B");}
+// else {
+//   alert("Equal input");
+// }
+
+//  33
+// let a = Number(prompt("A:"));
+// let b = Number(prompt("B:"));
+// if (a>b) {
+//   let t=a;
+//   a=b;
+//   b=t;
+// }
+// alert(a);
+// alert(b);
+
+//  34
+// let a = Number(prompt("A:"));
+// let b = Number(prompt("B:"));
+// if (a != b){
+//   let s = a+b;
+//   a =s;
+//   b=s;
+// }
+// else{
+//   a=0;
+//   b=0;
+// }
+// alert(a);
+// alert(b);
+
+//  35
+// let a = Number(prompt("A:"));
+// let b = Number(prompt("B:"));
+// if (a != b){
+//   if (a>b) b=a;
+//   else a=b; 
+// }
+// else{
+//   a=0;
+//   b=0;
+// }
+// alert(a);
+// alert(b);
+
+//  36
+// let a = Number(prompt("A:"));
+// let b = Number(prompt("B:"));
+// let c = Number(prompt("C:"));
+// if (a < b){
+//   if (a<c) alert(a);
+//   else alert(c);
+// } else {
+//   if (b<c) alert(b);
+//   else alert(c);
+// }
+
+//  37
+// let a = Number(prompt("A:"));
+// let b = Number(prompt("B:"));
+// let c = Number(prompt("C:"));
+// if (a < b){
+//   if (b<c) alert(b);
+//   else if (c<a) alert(a);
+//   else alert(c);
+// } else {
+//   if (a<c) alert(a);
+//   else if (c<b) alert(b);
+//   else alert(c);
+// }
+
+//  38
+// let a = Number(prompt("A:"));
+// let b = Number(prompt("B:"));
+// let c = Number(prompt("C:"));
+// if (a < b){
+//   if (a<c) {
+//     alert(a);
+//     if (c>b){
+//       alert(c);
+//     }
+//     else{
+//       alert(b);
+//     }
+//   }
+//   else {
+//     alert(c);
+//     alert(b);
+//   }
+// } else {
+//   if (b<c) {
+//     alert(b);
+//     if (c>a){
+//       alert(c);
+//     }
+//     else{
+//       alert(a);
+//     }
+//   }
+//   else {
+//     alert(c);
+//     alert(a);
+//   }
+// }
+
+//  39
+// let a = Number(prompt("A:"));
+// let b = Number(prompt("B:"));
+// let c = Number(prompt("C:"));
+// if (a < b){
+//   if (a<c) alert(b+c);
+//   else alert(a+b);
+// } else {
+//   if (b<c) alert(a+c);
+//   else alert(a+b);
+// }
+
+//  40
+// let a = Number(prompt("A:"));
+// let b = Number(prompt("B:"));
+// let c = Number(prompt("C:"));
+// if ((a != b) && (a!=c)){
+//   alert(a);
+// }
+// if ((b != a) && (b!=c)){
+//   alert(b);
+// }
+
+// if ((c != b) && (c!=a)){
+//   alert(c);
+// }
