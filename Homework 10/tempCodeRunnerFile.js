567be0892bd42a96aa6df1f0bde43064e0fe1e92
Valentine Yurchenko <45362610+VelHRH@users.noreply.github.com>
2022-10-24 12:21:40 +0300

    HV 10

---
+++ Homework 10/tempCodeRunnerFile.js
@@ -0,0 +1,10 @@
+function pow(x, n) {
+//   let res=1;
+//   for(let i=0; i<n; i++){
+//     res*=x;
+//   }
+//   return res;
+// }
+// let x = prompt("X:");
+// let n = prompt("N:")
+// alert(pow(x, n));
