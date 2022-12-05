31c08ccd3c76ccd86092fe3988c3a239ff08956b
Valentine Yurchenko <45362610+VelHRH@users.noreply.github.com>
2022-12-05 14:37:12 +0300

    Sliding animation finished

--- Homework 19/hv19/src/App.js
+++ Homework 19/hv19/src/App.js
@@ -3,17 +3,38 @@ import {
  ChevronDoubleRightIcon,
 } from "@heroicons/react/24/solid";
 import { useState } from "react";
-import { CSSTransition } from "react-transition-group";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 import "./index.css";
 
 function App() {
  const [page, setPage] = useState(1);
+ const [showbtn, setShowbtn] = useState(true);
+ const [direction, setDirection] = useState(true);
+
+ const btnLeftClickhandle = () => {
+  setPage(page - 1);
+  setDirection(false);
+  setShowbtn((showbtn) => !showbtn);
+  setTimeout(() => {
+   setShowbtn((showbtn) => !showbtn);
+  }, 300);
+ };
+
+ const btnRightClickhandle = () => {
+  setPage(page + 1);
+  setDirection(true);
+  setShowbtn((showbtn) => !showbtn);
+  setTimeout(() => {
+   setShowbtn((showbtn) => !showbtn);
+  }, 300);
+ };
+
  return (
   <>
    <CSSTransition
     in={page === 1}
-    classNames="alert"
     timeout={300}
+    classNames={direction ? "left" : "right"}
     unmountOnExit
    >
     <div
@@ -35,51 +56,63 @@ function App() {
      ></input>
     </div>
    </CSSTransition>
-
-   <div
-    className={`${
-     page !== 2 && "hidden"
-    } w-[90%] md:w-1/3 bg-lime-500 p-3 flex flex-col mb-2 rounded-2xl ml-[50%] translate-x-[-50%] text-xl font-semibold items-center`}
+   <CSSTransition
+    in={page === 2}
+    timeout={300}
+    classNames={direction ? "left" : "right"}
+    unmountOnExit
    >
-    <div className="text-slate-50 w-[90%] mr-2 self-left pl-2 pb-1 mt-2">
-     Имя
+    <div
+     className={`w-[90%] md:w-1/3 bg-lime-500 p-3 flex flex-col mb-2 rounded-2xl ml-[50%] translate-x-[-50%] text-xl font-semibold items-center`}
+    >
+     <div className="text-slate-50 w-[90%] mr-2 self-left pl-2 pb-1 mt-2">
+      Имя
+     </div>
+     <input
+      type="text"
+      className="w-[90%] rounded-xl focus:outline-lime-700 p-2 mb-3"
+     ></input>
+     <div className="text-slate-50 w-[90%] mr-2 self-left pl-2 pb-1 mt-2">
+      Фамилия
+     </div>
+     <input
+      type="text"
+      className="w-[90%] rounded-xl focus:outline-lime-700 p-2 mb-3"
+     ></input>
+     <div className="text-slate-50 w-[90%] mr-2 self-left pl-2 pb-1 mt-2">
+      Отчество
+     </div>
+     <input
+      type="text"
+      className="w-[90%] rounded-xl focus:outline-lime-700 p-2 mb-3"
+     ></input>
     </div>
-    <input
-     type="text"
-     className="w-[90%] rounded-xl focus:outline-lime-700 p-2 mb-3"
-    ></input>
-    <div className="text-slate-50 w-[90%] mr-2 self-left pl-2 pb-1 mt-2">
-     Фамилия
-    </div>
-    <input
-     type="text"
-     className="w-[90%] rounded-xl focus:outline-lime-700 p-2 mb-3"
-    ></input>
-    <div className="text-slate-50 w-[90%] mr-2 self-left pl-2 pb-1 mt-2">
-     Отчество
-    </div>
-    <input
-     type="text"
-     className="w-[90%] rounded-xl focus:outline-lime-700 p-2 mb-3"
-    ></input>
-   </div>
+   </CSSTransition>
 
    <div className="w-[90%] md:w-1/3 ml-[50%] translate-x-[-50%] flex justify-between">
     <button
-     onClick={() => page > 1 && setPage(page - 1)}
+     onClick={() => {
+      page > 1 && btnLeftClickhandle();
+     }}
      className={`${
       page < 2 &&
-      "bg-transparent text-transparent border-white hover:text-white cursor-default"
-     } bg-lime-500 text-white p-2 rounded-xl border-2 border-lime-500 hover:bg-transparent hover:text-lime-500`}
+      "bg-transparent text-transparent border-transparent hover:text-transparent cursor-default"
+     } ${!showbtn && "hidden"} ${
+      page >= 2 && "bg-lime-500 text-white border-lime-500 hover:text-lime-500"
+     } p-2 rounded-xl border-2 hover:bg-transparent`}
     >
      <ChevronDoubleLeftIcon className="w-6" />
     </button>
     <button
-     onClick={() => page < 5 && setPage(page + 1)}
+     onClick={() => {
+      page < 5 && btnRightClickhandle();
+     }}
      className={`${
       page > 4 &&
-      "bg-transparent text-transparent border-transparent hover:text-transparent"
-     } bg-lime-500 self-end text-white p-2 rounded-xl border-2 border-lime-500 hover:bg-transparent hover:text-lime-500`}
+      "bg-transparent text-transparent border-transparent hover:text-transparent cursor-default"
+     } ${!showbtn && "hidden"} ${
+      page <= 4 && "bg-lime-500 text-white border-lime-500 hover:text-lime-500"
+     } text-white p-2 rounded-xl border-2 hover:bg-transparent`}
     >
      <ChevronDoubleRightIcon className="w-6" />
     </button>
