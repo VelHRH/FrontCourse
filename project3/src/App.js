df41a272e2ed6e35b42c5825ad4306ebf0d25ee7
Valentine Yurchenko <45362610+VelHRH@users.noreply.github.com>
2022-12-09 16:28:42 +0300

    Router connected

--- project3/src/App.js
+++ project3/src/App.js
@@ -1,11 +1,20 @@
 import { Navbar } from "./components/Navbar";
 import Task1 from "./pages/Task1";
+import Task2 from "./pages/Task2";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
  return (
   <>
-   <Navbar />
-   <Task1 />
+   <BrowserRouter>
+    <Navbar />
+    <Routes>
+     <Route path="/" element={<Task1 />} />
+     <Route path="/quiz" element={<Task2 />} />
+     <Route path="/photos" element={<Task1 />} />
+     <Route path="*" element={<Task1 />} />
+    </Routes>
+   </BrowserRouter>
   </>
  );
 }
--- project3/src/components/Navbar.jsx
+++ project3/src/components/Navbar.jsx
@@ -1,17 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const Navbar = () => {
  return (
   <div className="flex w-[50%] h-20">
-   <button className="bg-pink-400 absolute font-bold left-28 top-5 uppercase p-3 text-3xl border-4 border-slate-900 shadow-[5px_5px] active:shadow-none active:ml-[5px] active:mt-[5px]">
-    Converter
-   </button>
-   <button className="bg-lime-400 absolute font-bold left-[50%] top-5 translate-x-[-50%] uppercase p-3 text-3xl border-4 border-slate-900 shadow-[5px_5px] active:shadow-none active:ml-[5px] active:mt-[5px]">
-    Quiz
-   </button>
-   <button className="bg-sky-400 absolute font-bold right-28 top-5 uppercase p-3 text-3xl border-4 border-slate-900 shadow-[5px_5px] active:shadow-none active:ml-[5px] active:mt-[5px]">
-    PhotoGramm
-   </button>
+   <Link to="/">
+    <button className="bg-pink-400 absolute font-bold left-28 top-5 uppercase p-3 text-3xl border-4 border-slate-900 shadow-[5px_5px] active:shadow-none active:ml-[5px] active:mt-[5px]">
+     Converter
+    </button>
+   </Link>
+   <Link to="/quiz">
+    <button className="bg-lime-400 absolute font-bold left-[50%] top-5 translate-x-[-50%] uppercase p-3 text-3xl border-4 border-slate-900 shadow-[5px_5px] active:shadow-none active:ml-[5px] active:mt-[5px]">
+     Quiz
+    </button>
+   </Link>
+   <Link to="/photos">
+    <button className="bg-sky-400 absolute font-bold right-28 top-5 uppercase p-3 text-3xl border-4 border-slate-900 shadow-[5px_5px] active:shadow-none active:ml-[5px] active:mt-[5px]">
+     PhotoGramm
+    </button>
+   </Link>
   </div>
  );
 };
