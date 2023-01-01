c435605c88bd05b0283aedf2e1d06c52ce3cf33a
Valentine Yurchenko <45362610+VelHRH@users.noreply.github.com>
2023-01-01 11:11:34 +0300

    Absolute to fixed

--- final_project/src/components/OneOfTwo/FullEvenView.jsx
+++ final_project/src/components/OneOfTwo/FullEvenView.jsx
@@ -15,16 +15,17 @@ export const FullEvenView = ({
 }) => {
  return (
   <>
+   {console.log(link)}
    <div
     onClick={() => setIsEventOpened(-1)}
-    className="absolute left-0 top-0 w-screen h-screen overscroll-none bg-slate-800 z-30 opacity-90"
+    className="fixed left-0 top-0 w-screen h-screen overscroll-none bg-slate-800 z-30 opacity-90"
    ></div>
    <img
     src={link}
     alt="Image"
-    className="w-[800px] h-[450px] object-cover absolute left-[50%] translate-x-[-50%] top-10 z-40"
+    className="w-[800px] h-[450px] object-cover fixed left-[50%] translate-x-[-50%] top-10 z-40"
    />
-   <div className="flex flex-col absolute bottom-20 left-[50%] translate-x-[-50%] z-50 items-center">
+   <div className="flex flex-col fixed bottom-20 left-[50%] translate-x-[-50%] z-50 items-center">
     <div className="text-yellow-400 text-4xl mb-4">{name}</div>
     <div className="flex">
      <div className="flex flex-col text-green-400 text-2xl items-center">
