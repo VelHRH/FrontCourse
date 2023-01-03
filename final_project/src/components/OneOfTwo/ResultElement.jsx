3fb5dd3771cfd7dea5245822fec276ec9faa0d5b
Valentine Yurchenko <45362610+VelHRH@users.noreply.github.com>
2023-01-03 14:35:47 +0300

    result page design

---
+++ final_project/src/components/OneOfTwo/ResultElement.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const ResultElement = ({ imgUrl, place }) => {
+ return (
+  <div className="w-full h-[200px] cursor-pointer hover:h-[350px] ease-in-out duration-300 border-b-2 border-slate-50 flex items-center justify-between">
+   <div className="text-3xl">{place}</div>
+   <img src={imgUrl} alt="Result" className="w-[95%] h-full object-cover" />
+  </div>
+ );
+};
--- final_project/src/pages/OneOfTwo/Results.jsx
+++ final_project/src/pages/OneOfTwo/Results.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
 import axios from "../../axios";
+import { ResultElement } from "../../components/OneOfTwo/ResultElement";
 
 const Results = () => {
  const results = useSelector((state) => state.results);
@@ -16,7 +17,42 @@ const Results = () => {
    })
    .finally(() => setIsLoading(false));
  }, []);
- return <>{console.log(results)}</>;
+
+ const filterRes = (res) => {
+  let sortedArray = [];
+  for (let i = res.length - 1; i >= 0; i--) {
+   let isMatch = false;
+   for (let j = 0; j < sortedArray.length; j++) {
+    if (res[i]._id === sortedArray[j]._id) {
+     isMatch = true;
+     break;
+    }
+   }
+   if (!isMatch) sortedArray.push(res[i]);
+  }
+  return sortedArray;
+ };
+ return (
+  <>
+   <div className="mt-10 w-[98%] md:w-[70%] ml-[50%] translate-x-[-50%] mb-10">
+    {isLoading ? (
+     <div>Loading...</div>
+    ) : (
+     <div className="flex w-full">
+      <div className="w-[70%]">
+       <div className="text-3xl text-center mb-2">
+        Your results for {results.res[0].subcategory}
+       </div>
+       {filterRes(results.res).map((result, index) => (
+        <ResultElement imgUrl={result.imgUrl} place={index + 1} />
+       ))}
+      </div>
+      <div className="w-[40%]"></div>
+     </div>
+    )}
+   </div>
+  </>
+ );
 };
 
 export default Results;
