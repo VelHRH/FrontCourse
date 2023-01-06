21ddfb54c26b4041b2dc59d5192db4642b876a02
Valentine Yurchenko <45362610+VelHRH@users.noreply.github.com>
2023-01-06 18:05:24 +0300

    Winners info on hover

--- final_project/src/pages/OneOfTwo/Me.jsx
+++ final_project/src/pages/OneOfTwo/Me.jsx
@@ -72,11 +72,20 @@ const Me = () => {
      </div>
      <div className="grid gap-4 grid-cols-10">
       {userData.data.winners?.map((winner) => (
-       <img
-        src={winner.imgUrl}
-        alt="Winner"
-        className="aspect-square object-cover rounded-lg"
-       />
+       <div className="flex relative group cursor-pointer bg-slate-900 rounded-lg">
+        <img
+         src={winner.imgUrl}
+         alt="Winner"
+         className="group-hover:opacity-0 aspect-square object-cover rounded-lg"
+        />
+        <span
+         class="group-hover:opacity-100 transition-opacity px-1 text-sm text-gray-100 rounded-md absolute left-1/2 
+    -translate-x-1/2 top-1/2 -translate-y-1/2 opacity-0"
+        >
+         {winner.date.slice(8, 10)}.{winner.date.slice(5, 7)}.
+         {winner.date.slice(0, 4)}
+        </span>
+       </div>
       ))}
      </div>
      <div className="text-3xl border-b-2 border-slate-600 p-2 my-4 mt-7">
